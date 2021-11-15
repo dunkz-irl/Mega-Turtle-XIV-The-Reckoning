7ce889d4a32d08fb48c5226ebce0ead3e8772451
frozentape <73592467+frozentape@users.noreply.github.com>
2021-11-15 16:53:11 +0000

    Add simple character controller

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Camera mainCamera;
+    CharacterController characterController;
+    public float MovementSpeed = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get Input Axis and create vector        
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 movementVector = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+        movementVector *= MovementSpeed;
+
+        // Move player controller
+        if (movementVector.magnitude >= 0.1f)
+        {
+            characterController.Move(movementVector * MovementSpeed * Time.deltaTime);            
+        }        
+    }
+}
