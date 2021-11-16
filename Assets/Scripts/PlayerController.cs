36ede7f175d569b0c599e334232849594cf0ebba
frozentape <73592467+frozentape@users.noreply.github.com>
2021-11-16 17:09:16 +0000

    Add double jump
    Still a couple of things to fix!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
-    Camera mainCamera;
-    CharacterController characterController;
+{   
     public float MovementSpeed = 1f;
     public float RotationSmoothTime = 0.1f;
-    float currentVelocity;
+    public float JumpHeight = 1f;
+    public float Gravity = -9.81f;
+    public bool CanDoubleJump = false;
+    public int MaximumJumps = 2;
+    public bool isGrounded;
 
+    Camera mainCamera;
+    CharacterController characterController;
+    float currentVelocity;
+    [SerializeField]Vector3 jumpVector = new Vector3();
+    int jumpCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -39,6 +46,40 @@ public class PlayerController : MonoBehaviour
             // Move player
             Vector3 movementVector = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             characterController.Move(movementVector.normalized * MovementSpeed * Time.deltaTime);            
-        }        
+        }
+
+        jumpVector.y += Gravity * Time.deltaTime; 
+        characterController.Move(jumpVector * Time.deltaTime); // For some reason this has to be called before accessing characterController.isGrounded or jumping is buggy when moving
+
+        isGrounded = characterController.isGrounded;
+        if (characterController.isGrounded && jumpVector.y <= 0)
+        {
+            jumpVector.y = 0f;
+            jumpCounter = 0; // Reset double jump counter
+        }
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            jumpVector.y += Mathf.Sqrt(JumpHeight * -2.0f * Gravity);
+            jumpCounter++;
+
+            Debug.Log("Jump");
+
+            // TODO: sound effect, animation
+        }
+
+        // Double Jump
+        if (Input.GetButtonDown("Jump") && !isGrounded)
+        {
+            if (CanDoubleJump && jumpCounter < MaximumJumps)
+            {
+                jumpVector.y += Mathf.Sqrt(JumpHeight * -2.0f * Gravity);
+                jumpCounter++;
+
+                // TODO: Particle effect, sound effect
+                // FIX: maths for double jump height, currently second jump force depends on height when double jump is started
+            }
+        }
     }
 }
