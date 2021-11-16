cdf7ff89af66cde6c5dab3b92f578173269134f3
frozentape <73592467+frozentape@users.noreply.github.com>
2021-11-16 15:24:31 +0000

    Add rotation and camera movement to player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     Camera mainCamera;
     CharacterController characterController;
     public float MovementSpeed = 1f;
+    public float RotationSmoothTime = 0.1f;
+    float currentVelocity;
 
 
     // Start is called before the first frame update
@@ -20,16 +22,23 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Get Input Axis and create vector        
-        float verticalInput = Input.GetAxis("Vertical");
-        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        Vector3 movementVector = new Vector3(horizontalInput, 0f, verticalInput).normalized;
-        movementVector *= MovementSpeed;
+        Vector3 inputVector = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+        inputVector *= MovementSpeed;
 
-        // Move player controller
-        if (movementVector.magnitude >= 0.1f)
+        // Player movement
+        if (inputVector.magnitude >= 0.1f)
         {
-            characterController.Move(movementVector * MovementSpeed * Time.deltaTime);            
+            // Rotate player to face in direction of movement
+            float targetAngle = Mathf.Atan2(inputVector.x, inputVector.z) * Mathf.Rad2Deg + mainCamera.transform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentVelocity, RotationSmoothTime); // To smooth player y rotation
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            // Move player
+            Vector3 movementVector = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(movementVector.normalized * MovementSpeed * Time.deltaTime);            
         }        
     }
 }
